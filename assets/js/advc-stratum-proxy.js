2269428905cb0f54800dda755ae55ee188bb343d
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2025-06-04 17:21:46 +0100

    Update advc-stratum-proxy.js

--- assets/js/advc-stratum-proxy.js
+++ assets/js/advc-stratum-proxy.js
@@ -1,17 +1,17 @@
 // advc-stratum-proxy.js
-const WebSocket = require('ws');
+const WebSocket = require('wss');
 const net = require('net');
 const url = require('url');
 
 const wss = new WebSocket.Server({ port: 3333 });
 
-wss.on('connection', (ws, req) => {
+wss.on('connection', (wss, req) => {
     const { query } = url.parse(req.url, true);
     const pool = (query.pool || '').split(':');
     const host = pool[0];
     const port = parseInt(pool[1] || '3333');
 
-    console.log(`Incoming WS client → connect to ${host}:${port}`);
+    console.log(`Incoming WSs client → connect to ${host}:${port}`);
 
     const stratum = net.connect(port, host, () => {
         console.log('Connected to mining pool');
@@ -19,20 +19,20 @@ wss.on('connection', (ws, req) => {
 
     // Forward Stratum data to browser
     stratum.on('data', (data) => {
-        ws.send(data.toString());
+        wss.send(data.toString());
     });
 
     // Forward browser data to pool
-    ws.on('message', (msg) => {
+    wss.on('message', (msg) => {
         stratum.write(msg + '\n');
     });
 
-    ws.on('close', () => {
+    wss.on('close', () => {
         stratum.end();
     });
 
     stratum.on('error', (err) => {
         console.error('Stratum error:', err.message);
-        ws.close();
+        wss.close();
     });
 });
